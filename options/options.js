fab3587c110c54780605af97c191a3d62c8c9f7e
Crowdbotics <team@crowdbotics.com>
2023-04-11 16:24:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "velocitymarkets-39838",
+  url: "https://velocitymarkets-39838.botics.co",
+  api: "https://velocitymarkets-39838.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
