84dc115ab736c859a6848300b4093dad5c897788
Slobodan Petrovic <slobodan.petrovic@crowdbotics.com>
2023-04-19 21:06:24 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "velocitymarkets-39838",
-  url: "https://velocitymarkets-39838.botics.co",
-  api: "https://velocitymarkets-39838.botics.co/api/v1"
-}
+  "name": "velocitymarkets-39838",
+  "url": "https://velocitymarkets-39838.botics.co",
+  "api": "https://velocitymarkets-39838.botics.co/api/v1",
+  "initialRoute": "largeGrid"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/large-grid/index.js
@@ -0,0 +1,89 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+
+const LargeGrid = () => {
+  const navigation = useNavigation();
+  return <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.gridContainer}>
+        <Pressable onPress={() => {
+        navigation.navigate("Login");
+      }}><View style={styles.row}>
+          <View style={styles.col}>
+            {
+              /* YOUR CODE HERE */
+            }
+          <Text style={styles.cALTSMHe}>Login</Text></View>
+          <View style={styles.col}>
+            {
+              /* YOUR CODE HERE */
+            }
+          </View>
+        </View></Pressable>
+        <View style={styles.row}>
+          <View style={styles.col}>
+            {
+            /* YOUR CODE HERE */
+          }
+          </View>
+          <View style={styles.col}>
+            {
+            /* YOUR CODE HERE */
+          }
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.col}>
+            {
+            /* YOUR CODE HERE */
+          }
+          </View>
+          <View style={styles.col}>
+            {
+            /* YOUR CODE HERE */
+          }
+          </View>
+        </View>
+      </View>
+    </ScrollView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  gridContainer: {
+    flex: 1
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+    marginHorizontal: 5,
+    padding: 5,
+    justifyContent: "center"
+  },
+  col: {
+    flex: 1,
+    backgroundColor: "pink",
+    marginHorizontal: 5,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  textHeading1: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  cALTSMHe: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
+export default LargeGrid;
